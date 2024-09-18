93546c3def83a512214cb38fbd6ab4b2c139333d
bighoc1 <bighoc1@gmail.com>
2024-09-18 23:32:42 +0200

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,12 @@
-# Écrivez votre code ici !
+nom = "hocine"
+age = 25
+taille = 1.85
+est_etudiant = true
+print(f"nom :" {nom})
+print(f"age :" {age})
+print(f"taille :" {taille})
+print(f"est un etudiant :" {est_etudiant})
+print(f"le type de nom : {type(nom)}")
+print(f"le type de l'age : {type(age)}")
+print(f"le type de taille : {type(taille)}")
+print(f"est etudiant {type(est_etudiant)}")
